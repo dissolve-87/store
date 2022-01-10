273c1ec7f93264d3f0cf16e471f9c23a9a7b83b8
dissolve-87 <narendran.dn97@gmail.com>
2022-01-11 01:59:52 +0530

    make db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+                      // args is itrorator
+                      //  skip first one  
+    let key = arguments.next().unwrap();
+                            // unrap() crash program
+    let val = arguments.next().expect("Please Enter key");
+                            // expect() Nice error message
+
+    let content = format!("{}\t{}\n",key,val);
+
+    std::fs::write("note.db",content).unwrap();
+    
+    println!("Key is :{}, value is {}",key,val);
+
+    let database = Database::create().expect("DB crashed,,,!");
+
+}
+
+struct Database{
+    map : std::collections::HashMap<String, String>,
+}
+
+impl Database {
+    fn create() -> Result<Database, std::io::Error>{
+
+        // let content = match std::fs::read_to_string("note.db") {
+        //     Ok(r) => r,
+        //     Err(err) => {
+        //         return Err(err);
+        //     }
+        // };
+
+        let content =  std::fs::read_to_string("note.db")?;
+
+        let mut dict = HashMap::new();
+        for line in content.lines(){
+                    // iterator
+            let mut collect = line.splitn(2,'\t');
+            let key = collect.next().expect("No key found");
+            let val = collect.next().expect("No value found");
+            dict.insert(key, val);
+        }
+
+        Ok(
+            Database{map: dict}
+        )
+    }
 }
